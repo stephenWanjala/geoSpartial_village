699cbc1fc26d44a3bc900f00789677e03da573a6
brandonladen <brandonladen486@gmail.com>
2024-03-29 07:51:16 +0300

    Styled the front table

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,23 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-
-    <div class="wrapper">
-
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <RouterLink class="navbar-brand" to="/">Table Visualization</RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about">Map</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
-
   <RouterView />
 </template>
+
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>geospatial mapping</h1>
   </div>
 </template>
 
