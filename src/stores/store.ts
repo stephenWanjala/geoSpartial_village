816e9f2f42e3b8e10a1c48cb911be3c49eeadbbc
wanjala stephen <stephenwanjala145@gmail.com>
2024-03-29 15:26:59 +0300

    update use Villages from Org Units Table

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,25 +1,27 @@
-// store.ts
-import { defineStore } from 'pinia'
-import { fetchVillages } from '@/viewmodel/viewmodels'
-import { ref } from 'vue'
-import type { Village } from '@/types/types'
+import { defineStore } from 'pinia';
+import { fetchVillages } from '@/viewmodel/viewmodels';
+import { ref } from 'vue';
+import type { Village, Pagination } from '@/types/types';
 
 export const useVillagesStore = defineStore({
   id: 'villages',
   state: () => ({
     villages: [] as Village[],
+    pagination: {} as Pagination,
     loading: ref(false),
     mError: ref<string | null>(null),
   }),
   actions: {
-    async fetchVillages() {
+    async fetchVillages(page: number = 1) {
       try {
-        this.loading= true;
-        this.villages = await fetchVillages();
+        this.loading = true;
+        const { villages, pagination } = await fetchVillages(page);
+        this.villages = villages;
+        this.pagination = pagination;
       } catch (error) {
         this.mError = (error as Error).message;
       } finally {
-        this.loading= false;
+        this.loading = false;
       }
     },
   },
