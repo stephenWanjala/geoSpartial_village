cf164fc08a85505eedad76bdee4610ae5b31628c
brandonladen <brandonladen486@gmail.com>
2024-03-30 07:57:57 +0300

    added select inputs

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -9,7 +9,54 @@ const markers = ref([
   // Add more markers with descriptions as needed
 ]);
 </script>
+<style lang="css" src="@/assets/main.css"></style>
 <template>
+<div>
+  <select id="county" name="county">
+  <option value="" disabled selected hidden>Select County</option>
+  <option value="kisumu">Kisumu</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+</select>
+
+<select id="sub-county" name="sub-county">
+  <option value="" disabled selected hidden>Select Subcounty</option>
+  <option value="kisumu">Kisumu</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+</select>
+
+<select id="ward" name="ward">
+  <option value="" disabled selected hidden>Select Ward</option>
+  <option value="kisumu">Kisumu</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+</select>
+
+<select id="selection" name="location">
+  <option value="" disabled selected hidden>Select Location</option>
+  <option value="kisumu">Kisumu</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+</select>
+
+<select id="Sublocation" name="Sublocation">
+  <option value="" disabled selected hidden>Select Sublocation</option>
+  <option value="kisumu">Kisumu</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+  <option value="homabay">Homabay</option>
+</select>
+</div>
   <div style="height: 100vh; width: 100%">
     <l-map ref="map" v-model:zoom="zoom" :center="[-0.5350427, 34.4530968]">
       <l-tile-layer
