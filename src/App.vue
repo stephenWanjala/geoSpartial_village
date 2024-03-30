e9ffe031d905b0800e8eea06f4a6cccab9a6d39d
wanjala stephen <stephenwanjala145@gmail.com>
2024-03-30 04:54:33 +0300

    map

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
-import { onMounted } from 'vue';
-import { RouterLink, RouterView } from 'vue-router';
+import { onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
 
 onMounted(() => {
-  const navbarToggle = document.querySelector('.navbar-toggler');
-  const navbarCollapse = document.querySelector('.navbar-collapse');
+  const navbarToggle = document.querySelector('.navbar-toggler')
+  const navbarCollapse = document.querySelector('.navbar-collapse')
 
   if (navbarToggle && navbarCollapse) {
     navbarToggle.addEventListener('click', () => {
-      navbarCollapse.classList.toggle('show');
-    });
+      navbarCollapse.classList.toggle('show')
+    })
   }
-});
+})
 
 </script>
-<style src="@/assets/main.css" lang="css"></style>
+<style lang="css" src="@/assets/main.css"></style>
 <template>
   <v-card>
     <v-layout>
@@ -23,21 +23,25 @@ onMounted(() => {
         expand-on-hover
         rail
       >
-      <v-list>
+        <v-list>
           <v-list-item
             prepend-avatar="https://training.digimal.uonbi.ac.ke/img/coat_of_arms.8d015f76.png"
+            style="font-weight: bold; color: #22c997; font-size: 24px; font-family: Inter, sans-serif;"
             subtitle="household distribution and registration"
             title="DigiMal-KE"
-            style="font-weight: bold; color: #22c997; font-size: 24px; font-family: Inter, sans-serif;"
           ></v-list-item>
-      </v-list>
+        </v-list>
 
 
-        <v-divider>  </v-divider>
+        <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <RouterLink to="/" class="nav-link"><v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item></RouterLink>
-          <RouterLink to="/map" class="nav-link"><v-list-item prepend-icon="mdi-map" title="map" value="map"></v-list-item></RouterLink>
+          <RouterLink class="nav-link" to="/">
+            <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+          </RouterLink>
+          <RouterLink class="nav-link" to="/map">
+            <v-list-item prepend-icon="mdi-map" title="map" value="map"></v-list-item>
+          </RouterLink>
           <v-list-item prepend-icon="mdi-car" title="Dist" value="dist"></v-list-item>
 
         </v-list>
@@ -48,9 +52,9 @@ onMounted(() => {
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
           <v-toolbar-title> DigiMal-KE</v-toolbar-title>
         </v-app-bar>
-        <v-container>
-          <RouterView/>
-        </v-container>
+
+        <RouterView />
+
       </v-main>
     </v-layout>
   </v-card>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,23 @@
-<template>
-  <v-container>
 
-  </v-container>
+<script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { ref } from 'vue'
+const zoom = ref(20);
+</script>
+
+<template>
+
+    <div style="height:100pc; width:100%">
+      <l-map ref="map" v-model:zoom="zoom" :center="[-0.5350427	,34.4530968]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
+
 </template>
+
+
