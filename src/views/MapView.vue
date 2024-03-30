c2666981874395dc1c3fcc9eb64c9482809c9159
wanjala stephen <stephenwanjala145@gmail.com>
2024-03-30 15:23:23 +0300

    sele

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,33 +1,43 @@
 <script setup lang="ts">
 import { useVillagesStore } from '@/stores/store'
 import { onMounted, ref } from 'vue'
-import type { Village } from '@/types/types'
+import type { County, Village } from '@/types/types'
+import 'leaflet/dist/leaflet.css'
+import { LMap, LMarker, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import { fetchCounties } from '@/viewmodel/MapViewmodel'
 
-const villagesStore = useVillagesStore();
-const villages = ref<Village[]>([]);
-const loading = ref<boolean>(false);
-const mError = ref<string | null>(null);
-import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+const villagesStore = useVillagesStore()
+const villages = ref<Village[]>([])
+const loading = ref<boolean>(false)
+const mError = ref<string | null>(null)
 
-const zoom = ref(13);
+const zoom = ref(8)
 
 onMounted(async () => {
-  loading.value = true;
+  loading.value = true
   try {
-    await villagesStore.fetchVillages();
-    villages.value = villagesStore.villages;
+    await villagesStore.fetchVillages()
+    villages.value = villagesStore.villages
   } catch (error) {
-    mError.value = (error as Error).message || 'An error occurred';
+    mError.value = (error as Error).message || 'An error occurred'
   } finally {
-    loading.value = false;
+    loading.value = false
   }
-});
+})
+const counties = ref<County[]>([])
+onMounted(async () => {
+  try {
+    counties.value = await fetchCounties()
+  } catch (error) {
+    mError.value = (error as Error).message || 'An error occurred'
+  }
+})
+
+
 
 </script>
 
 <template>
-  <v-container>
     <div class="text-center">
       <!-- Add any text or elements you want to display here -->
     </div>
@@ -43,11 +53,13 @@ onMounted(async () => {
           ></l-tile-layer>
           <!-- Use the template tag and apply the key attribute to it -->
           <template v-for="village in villages" :key="village.id">
-            <l-marker :lat-lng="[village.latitude, village.longitude]"></l-marker>
+            <l-marker :lat-lng="[village.latitude, village.longitude]">
+<!--              <l-popup>{{ village.name }}</l-popup>-->
+            </l-marker>
           </template>
         </l-map>
 
       </div>
     </div>
-  </v-container>
+
 </template>
