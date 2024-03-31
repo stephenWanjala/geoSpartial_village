0161c99e637fe6452d29fef35575b19a3c525aa6
brandonladen <brandonladen486@gmail.com>
2024-03-31 15:42:41 +0300

    feat: Implement search functionality in MapView component
    - Added a search bar with a search button to enable users to search for specific locations on the map.
    - Implemented a search algorithm that queries the OpenStreetMap Nominatim API based on the user's input.
    - Updated the map's zoom level and center coordinates to focus on the searched location when found.
    - Handled cases when no results are found and logged errors encountered during the search process.

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,19 +4,45 @@ import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
 import { ref, onMounted } from 'vue'
 import { useCountyStore } from '@/stores/mapstore'
 
+import axios from 'axios';
+
 const zoom = ref(10)
 const center = ref([-0.5350427, 34.4530968] as [number, number])
 const countyStore = useCountyStore()
 
+const searchQuery = ref('');
+
+const search = async () => {
+  try {
+    const response = await axios.get('https://nominatim.openstreetmap.org/search', {
+      params: {
+        q: searchQuery.value,
+        format: 'json',
+        limit: 1,
+      },
+    });
+    if (response.data.length > 0) {
+      const result = response.data[0];
+      center.value = [parseFloat(result.lat), parseFloat(result.lon)];
+      zoom.value = 15 ; // Adjust the zoom level as needed
+    } else {
+      // Handle case when no results are found
+      console.log('No results found');
+    }
+  } catch (error) {
+    console.error('Error searching:', error);
+  }
+};
+
 onMounted(async () => {
-  await countyStore.fetchAdministrativeUnitsByCounty(17) // replace 9 with the actual county id
+  await countyStore.fetchAdministrativeUnitsByCounty(35) // replace 9 with the actual county id
 })
 </script>
 
 <style lang="css" src="@/assets/main.css"></style>
 
 <template>
-  <v-app style="height: 500px; width: 100%">
+  <v-app style="height: 100vh; width: 100%">
     <v-progress-linear v-if="countyStore.loadingCoordinates" indeterminate class="bg-green"></v-progress-linear>
     <v-alert v-else-if="countyStore.mError" type="error">{{ countyStore.mError }}</v-alert>
     <Suspense>
@@ -52,8 +78,8 @@ onMounted(async () => {
 
         <!-- Floating search box -->
         <div class="search_bar">
-          <v-text-field v-model="searchQuery" placeholder="Search for village..." outlined></v-text-field>
-          <v-btn @click="search" text>Search</v-btn>
+            <v-text-field v-model="searchQuery" placeholder="Search..." outlined></v-text-field>
+            <v-btn @click="search" text>Search</v-btn>
         </div>
       </div>
       <template #fallback>
